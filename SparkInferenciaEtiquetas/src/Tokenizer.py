e7268a6dcbcab08631c8c72ca265f9d83014f1ad
Roberto Méndez <robzoros@gmail.com>
2017-07-15 21:29:33 +0200

    Ajustes y Web page

--- SparkInferenciaEtiquetas/src/Tokenizer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from nltk.tokenize.moses import MosesTokenizer, MosesDetokenizer
-import sys
-import inflect
-p = inflect.engine()
-
-for line in sys.stdin:
-  print " ".join(map(lambda word: p.singular_noun(word).encode('utf-8') if  p.singular_noun(word) else word.encode('utf-8'), MosesTokenizer().tokenize(line.decode('utf-8'))))
